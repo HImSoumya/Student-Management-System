9630b9c7999dce926949e6ab9e4c0705011d801e
HImSoumya <soumyarb2021@gmail.com>
2026-06-07 00:54:57 +0530

    project setup

---
+++ src/com/student_management/app/App.java
@@ -0,0 +1,7 @@
+package com.student_management.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Student Management System");
+    }
+}
